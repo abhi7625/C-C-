f70e8b06f5d003acf762964b29c80e58f99958a5
Abhishek Tak <abhishekt965@gmail.com>
2018-01-13 18:53:49 +0530

    Add files via upload

---
+++ facno.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int factno(int no)
+{
+
+int fact=1;
+    //int fact=1;
+    if(no==0)
+        return 1;
+    else if(no<3)
+        return no;
+    else
+    while (no!=0)
+        fact = fact*no;
+        no--;
+    return fact;
+}
+int main()
+{
+    int num,result;
+    cout<<" Enter number ";
+    cin>>num;
+    result=factno(num);
+    cout<<"Factorial is "<<result;
+}
